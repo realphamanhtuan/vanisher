0ea91f9ddf46e9df91bc924bbe6bf1aca9b14e76
thng1407 <thng1407@gmail.com>
2022-09-19 00:16:00 +0700

    Changed to base64 string receiving mechanism

--- backend/app.py
+++ backend/app.py
@@ -19,33 +19,52 @@ flask_cors.CORS(app)
 
 app.config['UPLOAD_FOLDER'] = config.IMG_PATH
 
-def GetFilesExtension(filename):
-    return filename.rsplit('.', 1)[-1].lower()
-def IsAnImagesName(filename):
-    return '.' in filename and \
-            GetFilesExtension(filename) in {'jpg', 'png'}
+import base64
+import io
+from PIL import Image
+
+
+def DecodeBase64Image(self, image_string):
+    try:
+        image = base64.b64decode(image_string)
+        img = Image.open(io.BytesIO(image))
+    except Exception:
+        Log('file is not valid base64 image')
+        return None
+    
+    if img.format.lower() == "png":
+        width, height = img.size
+        if width < config.MAX_IMAGE_SIZE and height < config.MAX_IMAGE_SIZE:
+            return img
+        else:
+            Log('image size exceeded, width and height must be less than 800 pixels')
+            return None
+    else:
+        Log('Image is not valid, only \'base64\' png images are valid')
+        return None
 
 def random_string(size=24, chars=string.ascii_letters + string.digits):
     return ''.join(random.choice(chars) for _ in range(size))
 
 @app.route("/api/upload", methods=['POST'])
 def Upload():
-    print(random_string())
-    if 'gt' not in request.files or 'mask' not in request.files:
-        return json.dumps(ERRORS_INPUT)
-    gt = request.files['gt']
-    mask = request.files['mask']
+    gt64 = request.data.get("gt")
+    mask64 = request.data.get("mask")
     # If the user does not select a file, the browser submits an
     # empty file without a filename.
-    if gt.filename == '' or mask.filename == '':
+    if gt64 == '' or gt64 == None or mask64 == '' or mask64 == None:
         flash('No selected file')
         return json.dumps(ERRORS_INPUT)
-    elif not IsAnImagesName(gt.filename) or not IsAnImagesName(mask.filename):
+    
+    gt = DecodeBase64Image(gt64)
+    mask = DecodeBase64Image(mask64)
+    
+    if gt == None or mask == None:
         return json.dumps(ERRORS_INPUT)
     else:
         gt_path, mask_path = random_string(), random_string()
-        gt.save(os.path.join(config.IMG_PATH, gt_path))
-        mask.save(os.path.join(config.IMG_PATH, mask_path))
+        gt.save(os.path.join(config.IMG_PATH, gt_path), format="png")
+        mask.save(os.path.join(config.IMG_PATH, mask_path), format="png")
         id = db.QueueAnImage(gt_path, mask_path, "127.0.0.1", "chrome")
         if id < 0:
             Log("Got negative id when adding entries to images table.")
